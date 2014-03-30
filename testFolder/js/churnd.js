d68b2cb7f95aa3e472b92558f13d3db379de49ab
Armaan <armaan110@gmail.com>
2014-03-30 19:07:08 -0400

    track +success

--- testFolder/js/churnd.js
+++ testFolder/js/churnd.js
@@ -10,25 +10,54 @@ Churnd.prototype.project_id = '';
 Churnd.prototype.login = function (email_id, account_name) {
 
     var sendData = 
-    	{
-    		email: "blah",
-    		account: account_name,
-            key: "5314182e2a0b6479c7000002"
-    	};
+        {
+            key: this.project_id, 
+            email: email_id,
+            account: account_name
+        };
+    var xhr = new XMLHttpRequest();   // new HttpRequest instance 
+    xhr.open("POST", "http://localhost:9292/api/login");
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(JSON.stringify(sendData));
+
+
+    xhr.onreadystatechange=function(){
+       if (xhr.readyState==4 && xhr.status==200){
+          console.log('xhr.readyState=',xhr.readyState);
+          console.log('xhr.status=',xhr.status);
+          console.log('response=',xhr.responseText);
+
+          // var data = $.parseJSON(xhr.responseText);
+          // var uploadResult = data['message']
+          // console.log('uploadResult=',uploadResult);
+
+          // if (uploadResult=='failure'){
+          //    console.log('failed to upload file');
+          //    displayError('failed to upload');
+          // }else if (uploadResult=='success'){
+          //    console.log('successfully uploaded file');
+          // }
+       }
+       else
+       {
+        console.log(xhr.statusText);
+       }
+    }
+  
+};
+
+
+Churnd.prototype.track = function (email_id, interaction_type) {
+
+    var sendData = 
+        {
+            key: this.project_id, 
+            email: email_id,
+            interaction: interaction_type
+        };
     var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-    xmlhttp.open("POST", "http://localhost:9292/api/login");
+    xmlhttp.open("POST", "http://localhost:9292/api/track");
     xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xmlhttp.send(JSON.stringify(sendData));
- // $.ajax({
- //        type: "POST",
- //        url: "http://localhost:9292/api/login",
- //        data: sendData,
- //        accepts: "application/json",
- //        dataType: "json",
- //        success: function(data){alert(data);},
- //        failure: function(errMsg) {
- //            alert(errMsg);
- //        }
- //  });
   
 };
--- testFolder/js/index.js
+++ testFolder/js/index.js
@@ -1,4 +1,13 @@
 
 
-var tracker = new Churnd('abcde');
-tracker.login("email","account"); 
+var tracker = new Churnd('5314182e2a0b6479c7000002');
+tracker.login("email@e.com","account"); 
+
+
+var tB = document.getElementById('testButton');
+
+tB.addEventListener('click', function(event) {
+  console.log(tB.id);
+ tracker.track("email@e.com",tB.id)
+
+});
